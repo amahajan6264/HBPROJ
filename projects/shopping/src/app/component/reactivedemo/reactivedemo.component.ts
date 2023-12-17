dab5052548df8649b69458fd1491eeee91fdcbbc
amahajan6264 <amahajan6264@gmail.com>
2023-12-17 19:25:20 +0530

    add and remove the field from form

--- projects/shopping/src/app/component/reactivedemo/reactivedemo.component.ts
+++ projects/shopping/src/app/component/reactivedemo/reactivedemo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder ,FormControl,Validator, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactivedemo',
   templateUrl: './reactivedemo.component.html',
@@ -9,15 +9,33 @@ export class ReactivedemoComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  public frmRegister=this.fb.group({
-     UserName : this.fb.control('',[Validators.required,Validators.maxLength(4),Validators.max(15)]),
-     Mobile : this.fb.control('',[Validators.required,Validators.pattern(/\+91\d{10}/)])
+  public frmRegister = this.fb.group({
+    UserName: this.fb.control('', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]),
+    Mobile: this.fb.control('', [Validators.required, Validators.pattern(/^\+91\d{10}$/)]),
+    Photos : this.fb.array([this.fb.control('')])
   });
 
-  get Accessor(){
+  public FormSubmit(formData : any) {
+    confirm(JSON.stringify(formData))
+    return 0;
+  }
+  get UserName() {
     return this.frmRegister.get('UserName') as FormControl;
   }
 
+  get Mobile() {
+    return this.frmRegister.get('Mobile') as FormControl;
+  }
+
+  get Photos(){
+    return this.frmRegister.get('Photos') as FormArray;
+  }
+  public AddControls(){
+    this.Photos.push(this.fb.control(''));
+  }
+  public RemoveClick(index : number){
+    this.Photos.removeAt(index);
+  }
   ngOnInit(): void {
 
   }
