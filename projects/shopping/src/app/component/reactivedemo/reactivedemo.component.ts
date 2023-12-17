60d981ece95eb74de54d65210ce900d4ca571339
amahajan6264 <amahajan6264@gmail.com>
2023-12-17 18:47:58 +0530

    send the data to api

--- projects/shopping/src/app/component/reactivedemo/reactivedemo.component.ts
+++ projects/shopping/src/app/component/reactivedemo/reactivedemo.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder ,FormControl,Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactivedemo',
   templateUrl: './reactivedemo.component.html',
   styleUrls: ['./reactivedemo.component.css']
 })
-export class ReactivedemoComponent {
+export class ReactivedemoComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  public frmRegister=this.fb.group({
+     UserName : this.fb.control('',[Validators.required,Validators.maxLength(4),Validators.max(15)]),
+     Mobile : this.fb.control('',[Validators.required,Validators.pattern(/\+91\d{10}/)])
+  });
+
+  get Accessor(){
+    return this.frmRegister.get('UserName') as FormControl;
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
